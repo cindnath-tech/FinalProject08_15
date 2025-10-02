417de389965a561c4048c2613544b0d4660a3f74
Inas Asami El <inasasami11@gmail.com>
2025-10-02 15:14:23 +0700

    inas merubah kode

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Ini baris pertama");
-        System.err.println("Ini inas");
+        System.out.println("Ini inas baru");
     }
 }
